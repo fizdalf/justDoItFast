ad80498656ccfefff0ace294723fff32937edfdc
fizdalf <fizdalf@gmail.com>
2024-05-15 11:46:35 +0100

    Add GetGameSessionPreviewQueryHandler in backend API
    This commit introduces a new Query Handler, GetGameSessionPreviewQueryHandler, in the backend API. The handler retrieves a game session's details including its id, the host player's name, and team details. This facilitates the fetching of a game session's overview in a structured format.

---
+++ apps/backend-api/src/game-session/application/get-game-session-preview/get-game-session-preview.query-handler.ts
@@ -0,0 +1,32 @@
+import {IQueryHandler, QueryHandler} from '@nestjs/cqrs';
+import {GetGameSessionPreviewQuery} from '../../domain/query/get-game-session-preview.query';
+import {GameSessionRepository} from '../../domain/repositories/game-session.repository';
+import {Inject, Injectable} from '@nestjs/common';
+import {GameSessionId} from '../../domain/valueObjects/GameSessionId';
+
+import {GameSessionPreview} from '@org/core/game-session/dto/gameSessionPreview';
+
+
+@QueryHandler(GetGameSessionPreviewQuery)
+@Injectable()
+export class GetGameSessionPreviewQueryHandler implements IQueryHandler<GetGameSessionPreviewQuery> {
+
+    constructor(@Inject(GameSessionRepository) private gameSessionRepository: GameSessionRepository) {
+    }
+
+    public async execute(query: GetGameSessionPreviewQuery): Promise<GameSessionPreview> {
+        const gameSession = await this.gameSessionRepository.findOneById(GameSessionId.fromValue(query.sessionId));
+
+        return {
+            id: gameSession.id.value,
+            hostPlayerName: gameSession.hostPlayerName().value,
+            teams: gameSession.teams.map(team => ({
+                id: team.id.value,
+                players: team.players.map(player => ({
+                    id: player.id.value,
+                    name: player.name.value,
+                })),
+            }))
+        };
+    }
+}
