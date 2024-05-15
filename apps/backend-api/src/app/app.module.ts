255b7a391b998a65f51563e39de011269fa87d8c
fizdalf <fizdalf@gmail.com>
2024-05-15 10:07:58 +0100

    Removed socket modules and services in backend app
    Deleted SocketModule, SocketService and SocketGateway from the backend application. These redundant files were removed to streamline the backend codebase and enhance maintainability. `-SocketModule` import is also removed from `app.module.ts`.

--- apps/backend-api/src/app/app.module.ts
+++ apps/backend-api/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
-import {SocketModule} from '../socket/socket.module';
 import {GameSessionModule} from '../game-session/game-session.module';
 
 @Module({
     imports: [
-        SocketModule,
         GameSessionModule,
     ],
     controllers: [AppController],
