53502d79179b34ce813ad919921a2d589f68554b
fizdalf <fizdalf@gmail.com>
2024-05-15 09:41:58 +0100

    Remove redundant session and identity effects in waiting room store
    The redundant effects 'setSessionIdFromRoute' and 'loadIdentity' were removed from the session join waiting room page store. While their invocations facilitate storing session and identity information, these operations are now managed elsewhere which makes these effects obsolete. This refactoring cleans up the code and enhances manageability.

--- apps/game/src/app/features/session-join-waiting-room-page/session-join-waiting-room-page.store.ts
+++ apps/game/src/app/features/session-join-waiting-room-page/session-join-waiting-room-page.store.ts
@@ -3,9 +3,8 @@ import {ComponentStore} from '@ngrx/component-store';
 import {GameSessionService} from '../../services/game-session/gameSessionService';
 import {ActivatedRoute} from '@angular/router';
 
-import {map, switchMap, tap, withLatestFrom} from 'rxjs';
+import {tap, withLatestFrom} from 'rxjs';
 import {IdentityService} from '../../services/identity-service/identity.service';
-import {fromPromise} from 'rxjs/internal/observable/innerFrom';
 import {GameSession} from '../../services/game-session/gameSession';
 
 
@@ -32,24 +31,10 @@ export class SessionJoinWaitingRoomPageStore extends ComponentStore<SessionJoinW
     readonly identity$ = this.select(state => state.identity);
     readonly session$ = this.select(state => state.session);
     //// Updaters ////
-    private setSession = this.updater((state, session: GameSession): SessionJoinWaitingRoomPageState => {
-        return {
-            ...state,
-            session
-        };
 
-    });
 
     //// Effects ////
-    private readonly setSessionIdFromRoute = this.effect(() => this.activatedRoute.params.pipe(
-        map(params => params['sessionId']),
-        switchMap(sessionId => this.sessionService.openSession()),
-        tap(session => this.setSession(session))
-    ));
 
-    private readonly loadIdentity = this.effect(() => fromPromise(this.identityService.getIdentity()).pipe(
-        tap(identity => this.patchState({identity}))
-    ));
 
     public readonly leaveSession = this.effect((trigger$) => {
         return trigger$.pipe(
